d3eb50f709d6c591507c48774ab7037d449dbe79
vb10 <vbacik.10@gmail.com>
2025-01-03 05:18:52 +0300

    Update dependencies, enhance localization, and modify project structure
    - Updated various package versions in pubspec.lock to their latest releases for improved functionality and security.
    - Added localization support in HomeAppBar and updated the supported locales in ProductLocalization.
    - Modified the lang.sh script to use 'flutter pub run' for generating localization keys, ensuring compatibility with Flutter projects.
    - Updated .gitignore to include new build-related directories.

--- script/lang.sh
+++ script/lang.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dart run easy_localization:generate  -O lib/product/init/language -f keys -o locale_keys.g.dart --source-dir asset/translations
+flutter pub run easy_localization:generate  -O lib/product/init/language -f keys -o locale_keys.g.dart --source-dir asset/translations
